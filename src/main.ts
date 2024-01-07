d5bd2f5642469dd855b52435288fdf870ef3f539
kennyhazzar <dmitriventro@gmail.com>
2024-01-08 01:50:50 +0700

    add port to main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { CommonConfigs } from '@core/types';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +19,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, docsConfig);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000); // TODO!: Add .env
+  const { port } = app.get(ConfigService).get<CommonConfigs>('common');
+
+  await app.listen(port);
 }
 bootstrap();
